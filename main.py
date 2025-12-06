c646a75a2d3e665e9f9cc7f7d0b4aaf014050e63
yugamax <ayp14111@gmail.com>
2025-12-07 00:04:58 +0530

    Comment out the main execution block in main.py

--- main.py
+++ main.py
@@ -217,6 +217,6 @@ async def websocket_endpoint(websocket: WebSocket):
         print("WebSocket got disconnected")
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8000))
+#     uvicorn.run("main:app", host="0.0.0.0", port=port)
