b8e8ce7be42c30f54efe782dd3e61743596bdc40
yugamax <ayp14111@gmail.com>
2025-12-06 23:34:19 +0530

    Update main.py to run Uvicorn on all interfaces instead of localhost

--- main.py
+++ main.py
@@ -219,4 +219,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="127.0.0.1", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
